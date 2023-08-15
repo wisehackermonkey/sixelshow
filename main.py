aa65cfeb3a67cf2990fa9ea803cd97722ad24336
Oran C <oranbusiness@gmail.com>
2023-08-14 22:33:44 -0600

    working with more stuff

--- main.py
+++ main.py
@@ -9,13 +9,6 @@ def show_image(filename):
     encoder.setopt(SIXEL_OPTFLAG_COLORS, "16")
     encoder.encode(filename)
 
-
-def show(stdscr, file):
-    stdscr.clear()
-    show_image(file)
-    stdscr.refresh()
-
-
 def main(stdscr):
     curses.curs_set(0)  # Hide the cursor
     curses.noecho()
@@ -26,9 +19,14 @@ def main(stdscr):
     png_files = [file for file in os.listdir(
         current_directory) if file.endswith('.png') or file.endswith(".jpg")]
     current_index = 0
+    if png_files:
+        filename = png_files[current_index]
+        stdscr.addstr( os.get_terminal_size().lines-2,0, f"Showing: {filename}", curses.A_BOLD)
+        show_image(filename)
+    else:
+        stdscr.addstr(1, 0, "No .png files found in the directory.")
 
     while True:
-        #    stdscr.clear()
         stdscr.addstr( os.get_terminal_size().lines-1,0
 , "Press 'Q' to quit")
 
@@ -38,23 +36,19 @@ def main(stdscr):
             show_image(filename)
         else:
             stdscr.addstr(1, 0, "No .png files found in the directory.")
-        # show_image(png_files[current_index])
-        # stdscr.refresh()
 
         key = stdscr.getch()
 
         if key == ord('q') or key == ord('Q'):
             break
         elif key == curses.KEY_RIGHT:
-            # stdscr.clear()
-            # stdscr.refresh()
+
             stdscr.clear()
             stdscr.refresh()
 
             current_index = (current_index + 1) % len(png_files)
         elif key == curses.KEY_LEFT:
-            # stdscr.clear()
-            # stdscr.refresh()
+
             stdscr.clear()
             stdscr.refresh()
 
